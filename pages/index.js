fdf700806251f9e07a8232eabbde1be4219d6912
venidium <venidium@gmail.com>
2023-02-03 15:36:46 +0900

    first001  commit

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home({ blog }) {
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
-
   return {
     props: {
       blog: data.contents,
